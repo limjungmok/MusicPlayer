4ac04432e09212a40d2d72bef7ef6d8f53f49bec
limjungmok <limjungmok@nts-corp.com>
2018-11-26 17:28:22 +0900

    #4 add markup for Recent Music

--- src/routes/container/Main.js
+++ src/routes/container/Main.js
@@ -4,11 +4,51 @@ import Footer from '../components/Footer';
 import util from '../../utils/util';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentMusicArray: []
+    };
+  }
+
+  componentDidMount() {
+    // fetch recentMusicArray
+  }
+
   render() {
     return (
       <div className="musicPlayer">
         <Header />
-        <div className="wrap">container</div>
+        <div className="wrap">
+          <section className="section">
+            <h2 className="section_title">
+              <a 
+                href={'#'} 
+                className="section_anchor">최신음악</a>
+            </h2>
+            <div className="recents">
+              {[...Array(10)].map((recent, i) => (
+                <a 
+                  href="#" 
+                  className="recent"
+                  key={i}
+                  >
+                  <figure className="recent_profile">
+                    <img 
+                      src={''}
+                      className="recent_img"
+                      alt='' 
+                    />
+                  </figure>
+                  <b className="recent_title">{'타이틀12341234'}</b>
+                  <i className="recent_artist">{'아티스트12342143'}</i>
+                </a>
+              ))}
+            </div>
+          </section>
+          <section className="section"></section>
+          <section className="section"></section>
+        </div>
         <Footer />
       </div>
     );
